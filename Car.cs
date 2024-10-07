01f1f4306b57e84d42a947089a98d220b9753ce6
MotieeAtefe <motiee1377@gmaile.com>
2024-10-07 10:35:40 +0330

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphic
+{
+    internal class Car
+    {
+        protected String Model;
+        protected String Name;
+        protected String color;
+        public void Start()
+        {
+            Console.WriteLine("The Car is Running... ");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("The Car is stoping...");
+
+        }
+
+
+    }
+}
