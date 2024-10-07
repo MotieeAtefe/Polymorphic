ba527b3fde346117a46aa304e7e9c715b149e8de
MotieeAtefe <motiee1377@gmaile.com>
2024-10-07 10:55:41 +0330

    ElectricCar create as child class for class of Car

---
+++ ElectricCar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphic
+{
+    internal class ElectricCar: Car
+    {
+        protected DateTime t;
+        protected double kwh;
+        public void MeasurementCapacityBatry(string t, double kwh)
+        {
+            this.t = DateTime.Parse(t);
+            this.kwh = kwh;
+            if(kwh < 2000) 
+            {
+                Console.WriteLine("Please charge Car");
+            }
+            else
+            {
+                Start();
+            }
+        }
+    }
+}
